6086ecc4c98a7f063ab42de28c0d300eedfc5149
PETER171 <leratokhambule171@gmail.com>
2022-08-20 15:20:25 +0200

    20082022

--- SeleniumPractice/src/test/java/Demo/FirstExample.java
+++ SeleniumPractice/src/test/java/Demo/FirstExample.java
@@ -19,28 +19,28 @@ import com.browserpage.HomePage;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class FirstExample {
-	
+
 	WebDriver driver;
-	
+
 	@BeforeTest
 	public void setUp() {
 		WebDriverManager.chromedriver().setup();
 		ChromeOptions options = new ChromeOptions();
 		driver = new ChromeDriver(options);
-		
-		//driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+
+		//driver.manage().timeouts().implicitlyWait();
 		driver.get("https://www.w3schools.com/");
 		driver.manage().window().maximize();
-		
+
 	}
-	
+
 	@Test
 	public void test1() throws InterruptedException {
-		
-//		//HomePage homePage = new HomePage(driver);
-//		Thread.sleep(10000);
-//		homePage.clickHTMLElement();
-//		Thread.sleep(5000);
+
+		HomePage homePage = new HomePage(driver);
+		Thread.sleep(10000);
+		homePage.clickHTMLElement();
+		Thread.sleep(5000);
 		driver.findElement(By.id("navbtn_tutorials")).click();
 		Thread.sleep(3000);
 		driver.findElement(By.xpath("//div[@class='w3-col l3 m6']//a[contains(text(),'Learn HTML')]")).click();
@@ -48,15 +48,15 @@ public class FirstExample {
 		driver.findElement(By.xpath("//div[@id='leftmenuinnerinner']//a[contains(text(),'HTML Forms')]")).click();
 		Thread.sleep(3000);
 		driver.findElement(By.xpath("(//div[@class='w3-example']//a[contains(text(),'Try it Yourself')])[1]")).click();
-		
-		
-		
+
+
+
 	}
-	
+
 	@AfterTest
 	public void AfterClass() {
 		driver.close();
 	}
-		
-	
+
+
 }
